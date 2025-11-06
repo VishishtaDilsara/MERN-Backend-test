da1d3d8a4e962ebef9a2396b24188102ad7c1ba8
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-11-06 11:16:34 +0530

    Bcrypt and login user created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,13 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
   const user = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
     role: req.body.role,
   });
 
@@ -21,4 +23,24 @@ export function createUser(req, res) {
         message: "User creation failed",
       });
     });
+}
+
+export function loginUser(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email }).then((user) => {
+    if (user == null) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+      if (isPasswordCorrect) {
+        res.json({
+          message: "Login successful",
+        });
+      } else {
+        res.status(401).json({ message: "Invalid password" });
+      }
+    }
+  });
 }
