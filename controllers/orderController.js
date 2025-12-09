8f1aee69926abe71b89f86690b8e9fc4882c3432
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-09 10:13:37 +0530

    Get orders function created

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -91,3 +91,23 @@ export async function createOrder(req, res) {
     return;
   }
 }
+
+export async function getOrders(req, res) {
+  if (req.user == null) {
+    res
+      .status(403)
+      .json({ message: "User must be logged in to place an order" });
+    return;
+  }
+  try {
+    if (req.user.role == "admin") {
+      const orders = await Order.find();
+      res.json(orders);
+    } else {
+      const orders = await Order.find({ email: req.user.email });
+      res.json(orders);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error retrieving orders", error: err });
+  }
+}
